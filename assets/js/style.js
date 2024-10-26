445ae20470a69300e5d2f77f25b080c2f7457e63
甜甜 <145662294+iosyyds@users.noreply.github.com>
2024-10-26 20:57:15 +0800

    更新 style.js

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,7 +3,7 @@ var url = window.location;
 var title = document.title;
 var ua = navigator.userAgent.toLowerCase();
 if (ua.indexOf("360se") > -1) {
-alert("由于360浏览器功能限制，请按 Ctrl+D 手动收藏！");
+alert("防止失联关注公众号：甜甜小窝");
 }
 else if (ua.indexOf("msie 8") > -1) {
 window.external.AddToFavoritesBar(url, title); //IE8
@@ -12,13 +12,13 @@ else if (document.all) {
 try{
 window.external.addFavorite(url, title);
 }catch(e){
-alert('加入收藏失败,请按 Ctrl+D 手动收藏!');
+alert('防止失联关注公众号：甜甜小窝');
 }
 }
 else if (window.sidebar) {
 window.sidebar.addPanel(title, url, "");
 }
 else {
-alert('加入收藏失败,请按 Ctrl+D 手动收藏!');
+alert('防止失联关注公众号：甜甜小窝');
 }
 }

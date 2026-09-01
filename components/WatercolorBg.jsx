82d399ff63f484d426fef0148130a54987bca982
甜甜 <145662294+iosyyds@users.noreply.github.com>
2026-09-02 01:35:52 +0800

    更新

--- components/WatercolorBg.jsx
+++
@@ -1,9 +0,0 @@
-export default function AmbientBg() {
-  return (
-    <div className="ambient-bg" aria-hidden="true">
-      <div className="glow glow-1"></div>
-      <div className="glow glow-2"></div>
-      <div className="glow glow-3"></div>
-    </div>
-  );
-}
